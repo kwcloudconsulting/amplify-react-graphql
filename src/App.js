35b9790b4b5c278b90050571370bad6a2c3688a1
kwcloudconsutling <kwcloudconsulting@gmail.com>
2022-10-31 20:31:39 -0500

    added code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
-import {
-  Button,
-  Flex,
-  Heading,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
+
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
@@ -21,7 +12,6 @@ import {
   Button,
   Flex,
   Heading,
-  Image,
   Text,
   TextField,
   View,
@@ -79,7 +69,7 @@ const App = ({ signOut }) => {
   }
 
   return (
-    <><View className="App">
+    <div><View className="App">
       <Heading level={1}>My Notes App</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
@@ -109,8 +99,7 @@ const App = ({ signOut }) => {
             key={note.id || note.name}
             direction="row"
             justifyContent="center"
-            alignItems="center"
-          >
+            alignItems="center">
             <Text as="strong" fontWeight={700}>
               {note.name}
             </Text>
@@ -122,36 +111,16 @@ const App = ({ signOut }) => {
         ))}
       </View>
       <Button onClick={signOut}>Sign Out</Button>
-    </View><View
+    </View>
+    <View
         name="image"
         as="input"
         type="file"
-        style={{ alignSelf: "end" }} /></>
+        style={{ alignSelf: "end" }} />
+    </div>
   );
 };
 
-{notes.map((note) => (
-  <Flex
-    key={note.id || note.name}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {note.image && (
-      <Image
-        src={note.image}
-        alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
-      />
-    )}
-    <Button variation="link" onClick={() => deleteNote(note)}>
-      Delete note
-    </Button>
-  </Flex>
-))}
+
 
 export default withAuthenticator(App);
